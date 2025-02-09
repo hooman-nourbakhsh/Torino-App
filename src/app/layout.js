7b62ea5bcd3e2f3068e0922a4b13236f47d57b8c
HoTaN <id4acc2014@protonmail.com>
2025-02-09 17:28:30 +0330

    feat: :sparkles: Integrated React Query and added QueryProvider
    I installed @tanstack/react-query and created a QueryProvider component to manage query caching. I wrapped the application with QueryProvider in layout.js to ensure React Query is available throughout the app. This setup improves data fetching and caching efficiency.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { yekanbakh, vazirmatn } from "@/utils/fonts";
+import QueryProvider from "@/providers/QueryProvider";
+
 import "@/app/globals.css";
 
 export const metadata = {
@@ -9,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`${vazirmatn.variable} ${yekanbakh.variable}`}>{children}</body>
+      <body className={`${vazirmatn.variable} ${yekanbakh.variable}`}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
